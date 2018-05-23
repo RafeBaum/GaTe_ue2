62898d047c5d853c9236b1fa6279355e73431c3a
SOFIEAR\Baum <rafe96@gmx.de>
2018-05-23 14:24:03 +0200

    UpToDate_23052018

---
+++ New Unity Project/Assets/HTW/Scripts/EnemyControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+    Rigidbody2D enemyRB;
+    public float speed, jumpspeed;
+    int maxHp = 100;
+    int currHp;
+    float hitRange, atkDelay, atkTimer;
+	// Use this for initialization
+	void Start () {
+        enemyRB = GetComponent<Rigidbody2D>();
+        currHp = maxHp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage(int dmg)
+    {
+        Debug.Log("enemy takeDMG");
+        currHp -= dmg;
+        if(currHp<= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- New Unity Project/Assets/HTW/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/HTW/Scripts/PlayerMovement.cs
@@ -3,38 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
+    
 	Rigidbody2D rbPlayer;
 	Vector2 move;
+    public Collider2D atkTrigger;
 	public bool grounded = false;
 	public float speed;
 	public float jumpspeed;
+    public int maxHP = 3;
+    int currHP;
+    public float atkRate, atkCD;
+    bool lookDir;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		rbPlayer = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		move = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0);
-		rbPlayer.AddForce (move * speed);
-		if (grounded) {
-			if (Input.GetKeyDown ("space")) {
-				rbPlayer.AddForce (new Vector2 (0, 1) * jumpspeed);
-				grounded = false;
-				Debug.Log ("jump");
-			}
-		}
-	}
+        if (move.x > 0)
+        {
+            lookDir = true;
+        }
+        else
+            lookDir = false;
+     //TODO 
+     //umdrehen richten 
+     //atktrigger richtig einbinden
+		
+            if (atkRate > 0)
+                atkRate -= Time.deltaTime;
+
+            if (atkRate < 0)
+                atkRate = 0;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (atkRate == 0)
+            {
+                    Attack();
+                    atkRate = atkCD;
+                Debug.Log(atkRate);
+            }
+        }
+    }
+	
 
 	void FixedUpdate(){
-	}
+        rbPlayer.AddForce(move * speed*Time.deltaTime);
+        if (grounded)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rbPlayer.AddForce(new Vector2(0, 1) * jumpspeed);
+                grounded = false;
+               
+            }
+        }
 
-	void OnCollisionStay2D(Collision2D col){
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.CompareTag("Wall")){
 			grounded = true;
 		}
 	}
 
+
+    void Attack()
+    {
+       
+    }
 }
