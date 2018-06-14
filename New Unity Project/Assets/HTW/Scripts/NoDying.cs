c00b364954b852846328dc82cd7fc02822031975
SOFIEAR\Baum <rafe96@gmx.de>
2018-06-14 22:56:54 +0200

    trash

---
+++ New Unity Project/Assets/HTW/Scripts/NoDying.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoDying : MonoBehaviour {
+    public Text nopeText;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
